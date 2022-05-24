84403f28205738fdfd7bc2b5f4ff6b06c8ce232a
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-05-24 10:43:38 +0200

    Initial commit

---
+++ src/main/java/com/adri/bs4_1/Ejerciciobs41SpringApplication.java
@@ -0,0 +1,13 @@
+package com.adri.bs4_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejerciciobs41SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejerciciobs41SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adri/bs4_1/Ejerciciobs41SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adri.bs4_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerciciobs41SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
